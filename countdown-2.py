cb634ff022ebb8efe3ae01f59027507227326b6b
MarekFox <marek.f8x@gmail.com>
2025-12-11 11:09:40 +0100

    Fix: Implement 'stty cols' in countdown-2.py to force larger figlet font rendering on mobile devices.TARGET_COLUMNS = 35 and FIGLET_FONT = 'big'.

--- countdown-2.py
+++ countdown-2.py
@@ -2,14 +2,16 @@ from datetime import datetime, timedelta
 import time
 from colorama import Fore, init
 import pyfiglet
-import os # NOWY IMPORT
+import os 
 
 # 1. Ustawienia
 # Czcionka "doom" jest bardzo szeroka i blokowa
 # Czcionka "big" jest duża i klasyczna
 # jeszcze jest "banner"
-FIGLET_FONT = 'doom' 
-TARGET_COLUMNS = 5 # <<<<< KLUCZOWA ZMIENNA: Wymuś mniejszą liczbę kolumn (np. 55 zamiast 108)
+
+FIGLET_FONT = 'big' 
+TARGET_COLUMNS = 35 # <<< OPTYMALNA WARTOŚĆ WYMUSZAJĄCA MAKSYMALNY ROZMIAR
+ORIGINAL_COLUMNS = 108 # Twoja domyślna szerokość Termux
 
 # Inicjalizacja
 init(autoreset=True)
@@ -17,33 +19,36 @@ init(autoreset=True)
 # Ustawienie celu
 new_year = datetime(datetime.now().year + 1, 1, 1)
 
-# Ustawienie szerokości kolumn (zanim rozpocznie się pętla)
-# Ta komenda wymusi na Termux renderowanie znaków szerzej, by zmieściły się w 55 kolumnach
+# Ustawienie szerokości kolumn
 os.system(f"stty cols {TARGET_COLUMNS}") 
 
+try:
+    while datetime.now() < new_year:
+        # Calculate remaining time
+        remaining = new_year - datetime.now()
+        remaining_str = str(remaining).split(".")[0] 
+        
+        # Generowanie figlet
+        formatted_time = pyfiglet.figlet_format(remaining_str, font=FIGLET_FONT)
 
-while datetime.now() < new_year:
-    # ... (reszta obliczeń czasu bez zmian)
-    remaining = new_year - datetime.now()
-    remaining_str = str(remaining).split(".")[0] 
+        # Czyszczenie ekranu
+        print("\033[2J", end="") 
+
+        # Wyświetlanie
+        print(Fore.GREEN + formatted_time)
+
+        time.sleep(1)
+        
+finally:
+    # Ten blok GWARANTUJE, że komenda zostanie wykonana nawet po przerwaniu skryptu (np. Ctrl+C)
     
-    # 2. Generowanie figlet z czcionką 'doom'
-    formatted_time = pyfiglet.figlet_format(remaining_str, font=FIGLET_FONT)
+    # 2. Przywrócenie oryginalnej szerokości terminala
+    os.system(f"stty cols {ORIGINAL_COLUMNS}") 
+    
+    # Czyszczenie ekranu po zakończeniu
+    print("\033[2J", end="")
 
-    # Czyszczenie ekranu (lepiej działa \033[2J w Termux)
-    print("\033[2J", end="") 
-
-    # Wyświetlanie
-    print(Fore.GREEN + formatted_time)
-
-    time.sleep(1)
-
-# Czyszczenie ekranu po zakończeniu
-print("\033[2J", end="")
-
-# Wypisanie wiadomości końcowej
-new_year_msg = pyfiglet.figlet_format("Happy New Year!", font=FIGLET_FONT)
-print(Fore.YELLOW + new_year_msg)
-
-# *** ODCZYTANIE ZMIAN: Przywrócenie oryginalnej szerokości terminala ***
-os.system("stty cols 108") # Przywrócenie kolumn do domyślnej wartości (108)
+    # Wypisanie wiadomości końcowej (może być uruchomiona, jeśli pętla dobiegła końca)
+    if datetime.now() >= new_year:
+        new_year_msg = pyfiglet.figlet_format("Happy New Year!", font=FIGLET_FONT)
+        print(Fore.YELLOW + new_year_msg)
