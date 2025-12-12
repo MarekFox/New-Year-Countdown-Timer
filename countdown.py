358587a820f723d8712e7f54c9cd6a655094a0e7
MarekFox <marek.f8x@gmail.com>
2025-12-12 08:51:27 +0100

    Operations on files.

--- countdown.py
+++ countdown.py
@@ -2,33 +2,54 @@ from datetime import datetime, timedelta
 import time
 from colorama import Fore, init
 import pyfiglet
+import os 
 
-# Initialize colorama
+# 1. Ustawienia
+# Czcionka "doom" jest bardzo szeroka i blokowa
+# Czcionka "big" jest duża i klasyczna
+# jeszcze jest "banner"
+
+FIGLET_FONT = 'big' 
+TARGET_COLUMNS = 35 # <<< OPTYMALNA WARTOŚĆ WYMUSZAJĄCA MAKSYMALNY ROZMIAR
+ORIGINAL_COLUMNS = 108 # Twoja domyślna szerokość Termux
+
+# Inicjalizacja
 init(autoreset=True)
 
-# Set New Year's target
+# Ustawienie celu
 new_year = datetime(datetime.now().year + 1, 1, 1)
+# new_year = datetime(2025, 12, 12)
 
-while datetime.now() < new_year:
-    # Calculate remaining time
-    remaining = new_year - datetime.now()
+# Ustawienie szerokości kolumn
+os.system(f"stty cols {TARGET_COLUMNS}") 
 
-    # Format remaining time as a string
-    remaining_str = str(remaining).split(".")[0]  # Remove microseconds for clarity
-    formatted_time = pyfiglet.figlet_format(remaining_str)
+try:
+    while datetime.now() < new_year:
+        # Calculate remaining time
+        remaining = new_year - datetime.now()
+        remaining_str = str(remaining).split(".")[0] 
+        
+        # Generowanie figlet
+        formatted_time = pyfiglet.figlet_format(remaining_str, font=FIGLET_FONT)
 
-    # Clear the screen (works on most terminals)
-    print("\033c", end="")
+        # Czyszczenie ekranu
+        print("\033[2J", end="") 
 
-    # Display remaining time in figlet font
-    print(Fore.GREEN + formatted_time)
+        # Wyświetlanie
+        print(Fore.GREEN + formatted_time)
 
-    # Delay for 1 second
-    time.sleep(1)
+        time.sleep(1)
+        
+finally:
+    # Ten blok GWARANTUJE, że komenda zostanie wykonana nawet po przerwaniu skryptu (np. Ctrl+C)
+    
+    # 2. Przywrócenie oryginalnej szerokości terminala
+    os.system(f"stty cols {ORIGINAL_COLUMNS}") 
+    
+    # Czyszczenie ekranu po zakończeniu
+    print("\033[2J", end="")
 
-# Clear the screen one last time
-print("\033c", end="")
-
-# Print Happy New Year message
-new_year_msg = pyfiglet.figlet_format("Happy New Year!")
-print(Fore.YELLOW + new_year_msg)
+    # Wypisanie wiadomości końcowej (może być uruchomiona, jeśli pętla dobiegła końca)
+    if datetime.now() >= new_year:
+        new_year_msg = pyfiglet.figlet_format("Happy New Year!", font=FIGLET_FONT)
+        print(Fore.YELLOW + new_year_msg)
